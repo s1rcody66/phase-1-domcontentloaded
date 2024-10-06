92355b43e88d5f8d71d75da262e42b0d7794468e
s1rcody66 <cody200600@gmail.com>
2024-10-06 14:50:19 +0300

    completelab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => updateDOM());
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!";
+
+}
